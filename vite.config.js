cb24da1076ce5f54d188f0bbfb090cb962f0bd06
KevinDallancon <kevin.dallancon@hotmail.fr>
2025-06-18 21:23:22 +0200

    build package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/Modal.jsx',
+      name: 'HRnetModal',
+      fileName: 'hrnet-modal'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
